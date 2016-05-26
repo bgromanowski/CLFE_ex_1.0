8c1873fd4b3dd90154a025ce0fe134b80c3c63f5
bgromanowski <blazej.romanowski@gmail.com>
2016-05-26 21:56:31 +0200

    Zadanie8 done again

--- js/zadanie8.js
+++ js/zadanie8.js
@@ -22,3 +22,27 @@ for(var i=0; i<n; i = i+1){
   console.log(line);
 }
 */
+
+var n = 5;
+
+console.log("***Pętle zależne***");
+var line1 = "";
+for (var i = 1; i <= n; i++) {
+  line1 = "";
+  for (var j = 1; j <= i; j++) {
+    line1 = line1 + " * ";
+  };
+  console.log(line1);
+};
+
+console.log("***Pętle niezależne***");
+var line2 = "";
+for (var i = 1; i <= n; i ++){
+  line2 = "";
+  for (var j = 1; j <= n ; j++) {
+    if (j <= i) {
+      line2 = line2 + " * ";
+    };
+  };
+  console.log(line2);
+};
