a3f11c94acac03009a26cfd08475a25f9805553c
bgromanowski <blazej.romanowski@gmail.com>
2016-05-26 18:14:59 +0200

    Zadanie3 done again

--- js/zadanie3.js
+++ js/zadanie3.js
@@ -1,5 +1,5 @@
 console.log("zadanie3");
-
+/*
 var points = 58;
 
 if(points <0){
@@ -19,3 +19,24 @@ if(points <0){
 }else{
   console.log("zla liczba");
 }
+*/
+
+var points = 58;
+
+if (points < 0) {
+  console.log("BŁĄÐ!");
+} else if (points <= 39) {
+  console.log("NDST");
+} else if (points <= 54) {
+  console.log("DOP");
+} else if (points <= 69) {
+  console.log("DST");
+} else if (points <= 84) {
+  console.log("DOB")
+} else if (points <= 98) {
+  console.log("BDB");
+} else if (points <= 100) {
+  console.log("CEL");
+} else {
+  console.log("zła liczba")
+};
