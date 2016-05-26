5477c45ff3ce0c40ffa262cca6c6e208b58fd241
bgromanowski <blazej.romanowski@gmail.com>
2016-05-26 19:25:53 +0200

    Zadanie4 done again

--- js/zadanie4.js
+++ js/zadanie4.js
@@ -1,5 +1,5 @@
 console.log("zadanie4");
-
+/*
 var x = 9;
 
 var silnia1 = 1;
@@ -18,3 +18,22 @@ for(var j=1; j<=x; j=j+1){
 }
 
 console.log("Silnia z " + x + " wyliczona w pętli for = " + silnia2);
+*/
+
+var x = 5;
+
+console.log("#####Silnia WHILE#####");
+var i = 1;
+var silnia1 = 1;
+while (i <= x) {
+  silnia1 = i * silnia1;
+  i++;
+};
+console.log("Silnia z liczby " + x + " wyliczona w pętli WHILE wynosi " + silnia1);
+
+console.log("#####Silnia FOR#####");
+var silnia2 = 1;
+for (var j = 1; j <= x; j++) {
+  silnia2 = j * silnia2;
+};
+console.log("Silnia z liczby " + x + " wyliczona w pętli FOR wynosi " + silnia2);
